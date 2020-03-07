c9df7425a4d39d33c69cbb5498e0b6c4c3ebe1fe
angeliz_r <reyesangelize@gmail.com>
2020-03-08 00:06:12 +0800

    added SFX to bombs, added chest bomb icon to all bombs that arent explosions, added newspikes (not used yet)

--- UnityGameJam2020/Assets/Scripts/Bomb.cs
+++ UnityGameJam2020/Assets/Scripts/Bomb.cs
@@ -18,21 +18,40 @@ public class Bomb : MonoBehaviour
 
 
     [SerializeField]private PlayerType _playerType;
-
-  
-
+    private AudioController _audio;
     private void Start() {
+        _audio = GetComponent<AudioController>();
         Invoke("Explode", _explodeTime);
-        
 
-       
     }
 
     public void SetType(PlayerType t) {
         _playerType = t;
     }
 
+    void SelectAudio()
+    {
+        //FOR AUDIO
+        if (_bombType == Bomb.BombType.CROSS && _playerType == PlayerType.MAN)
+        {
+            _audio.PlaySoundEffect(SFXCollection.chainsaw_sfx);
+        }
+        else if (_bombType == Bomb.BombType.CROSS && _playerType == PlayerType.NATURE)
+        {
+            _audio.PlaySoundEffect(SFXCollection.tsunami_sfx);
+        }
+        else if (_bombType == Bomb.BombType.ROUND && _playerType == PlayerType.MAN)
+        {
+            _audio.PlaySoundEffect(SFXCollection.sparks_sfx);
+        }
+        else if (_bombType == Bomb.BombType.ROUND && _playerType == PlayerType.NATURE)
+        {
+            _audio.PlaySoundEffect(SFXCollection.earthquake_sfx);
+        }
+
+    }
     void Explode() {
+        SelectAudio();
         if (_bombType == BombType.CROSS) {
             for (int i = 0; i < 4; i++) {
                 var b = Instantiate(_crossProjectile, transform.position, Quaternion.identity);
@@ -41,7 +60,8 @@ public class Bomb : MonoBehaviour
                 b.transform.localEulerAngles = transRot.localEulerAngles;
                 b.GetComponent<BombExplosion>().SetBomb(BombType.CROSS);
                 b.GetComponent<BombExplosion>().SetType(_playerType);
-            }  
+            }
+           
         }
 
         if (_bombType == BombType.ROUND) {
