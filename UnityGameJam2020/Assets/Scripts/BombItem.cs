ee28548f89cc61e5843a62968d3006b8fdd5e9a3
Adam Parico <adamparico.taktyl@gmail.com>
2020-03-07 12:14:21 +0800

    add: Bomb Scripts, Spawn, Bomb, BombExplosion

---
+++ UnityGameJam2020/Assets/Scripts/BombItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class BombItem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            collision.GetComponent<BombPlanter>().SetHasBomb(true);
+        }
+    }
+}
---
+++ UnityGameJam2020/Assets/Scripts/BombPlanter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerType {
+    NATURE, MAN
+}
+
+//Part of Player Component
+public class BombPlanter : MonoBehaviour
+{
+    [SerializeField] PlayerType _playerType;
+
+    [SerializeField]
+    private GameObject _roundBomb;
+    [SerializeField]
+    private GameObject _crossBomb;
+
+    private bool _hasBomb = false;
+    private DualShock4Input ds;
+
+
+    private void OnEnable() {
+        _hasBomb = false;
+    }
+
+    private void Start() {
+        ds = GetComponent<DualShock4Input>();
+    }
+
+    public void SetHasBomb(bool b) {
+        _hasBomb = b;
+    }
+
+    public void PlantABomb() {
+        if (_hasBomb) {
+            if (ds.GetButtonDown(ControlCode.Circle)) {
+                // Plant Round Bomb
+                // SetBomb(_playerType, _roundBomb);
+            }
+            if (ds.GetButtonDown(ControlCode.Triangle)) {
+                // Plant Cross Bomb
+                // SetBomb(_playerType, _crossBomb);
+            }
+        }
+    }
+
+    private void SetBomb(PlayerType type, GameObject bomb) {
+        var b = Instantiate(bomb, transform.position, Quaternion.identity);
+        // b.GetComponent<Bomb>().SetType(type)
+        _hasBomb = false;
+    }
+}
+
+
