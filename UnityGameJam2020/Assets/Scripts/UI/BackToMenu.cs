4a009b30f5b7ff535e3518d199d586bbf3fb88c0
angeliz_r <reyesangelize@gmail.com>
2020-03-07 14:21:38 +0800

    added sfx, added back to menu, added round tutorial ish thing

---
+++ UnityGameJam2020/Assets/Scripts/UI/BackToMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BackToMenu : MonoBehaviour
+{
+    private AudioController _audio;
+    private void Awake()
+    {
+        _audio = GetComponent<AudioController>();
+    }
+    public void ToMainMenu()
+    {
+        _audio.PlaySoundEffect(SFXCollection.click);
+        SceneManager.LoadScene(0);
+    }
+
+}
